f515ef9fb4f6c82916c54f8f6505a96e1645103f
Joe Stump <joe@stu.mp>
2026-08-03 18:48:10 +0100

    chore(ui): gofumpt the question dialog textarea import swap

--- internal/ui/dialog/question_editor.go
+++ internal/ui/dialog/question_editor.go
@@ -5,10 +5,10 @@ import (
 	"strings"
 
 	"charm.land/bubbles/v2/key"
-	"github.com/charmbracelet/crush/internal/ui/textarea"
 	tea "charm.land/bubbletea/v2"
 	"charm.land/lipgloss/v2"
 	"github.com/charmbracelet/crush/internal/ui/styles"
+	"github.com/charmbracelet/crush/internal/ui/textarea"
 	uv "github.com/charmbracelet/ultraviolet"
 )
 
